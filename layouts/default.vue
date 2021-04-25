c289463ff72553b6384d62f1e005f6be594be5dd
mdjamilaj <mdjamilaj1@gmail.com>
2021-04-25 12:44:45 +0600

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,541 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list-item class="text-center mt-2">
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            <img src="~/assets/img/mlogo.svg" class="text-center" height="40" />
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list class="mt-4">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+          class="v-list-item-custom"
+        >
+          <v-list-item-action class="v-list-item__action-custom">
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-btn icon size="28" @click.stop="drawer = !drawer">
+        <v-icon>mdi-text</v-icon>
+      </v-btn>
+      <v-spacer class="text-center justify-center mt-2">
+        <v-text-field
+          outlined
+          dense
+          rounded
+          append-icon="mdi-magnify"
+          placeholder="Search someting were"
+          hide-details
+          class="my-sm-0 my-3 mx-auto"
+          style="max-width: 300px"
+        >
+        </v-text-field>
+      </v-spacer>
+
+      <!-- <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      :nudge-width="200"
+      :nudge-left="200"
+      :offset-overflow="true"
+      :position-x="5000"
+      :position-y="150"
+      :transition="true"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="indigo"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Menu as Popover
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-list>
+          <v-list-item>
+            <v-list-item-avatar>
+              <img
+                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                alt="John"
+              >
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>John Leider</v-list-item-title>
+              <v-list-item-subtitle>Founder of Vuetify</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn
+                :class="fav ? 'red--text' : ''"
+                icon
+                @click="fav = !fav"
+              >
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch
+                v-model="message"
+                color="purple"
+              ></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable messages</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-action>
+              <v-switch
+                v-model="hints"
+                color="purple"
+              ></v-switch>
+            </v-list-item-action>
+            <v-list-item-title>Enable hints</v-list-item-title>
+          </v-list-item>
+        </v-list>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            text
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click="menu = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-menu> -->
+      <!-- Message Start -->
+      <v-menu
+        :close-on-content-click="false"
+        :nudge-width="350"
+        offset-x
+        max-width="350"
+        min-width="350"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="black"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            icon
+            class="white mr-5 btn-shadow"
+          >
+            <v-badge
+              v-if="notificationUnseen > 0"
+              :content="notificationUnseen"
+              class="badge-notification"
+              color="#31B8B6"
+            ></v-badge>
+            <v-icon size="24">mdi-email-outline</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card id="notification">
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-regular subtitle-1 text-center"
+                  >10 new messages</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list
+            three-line
+            class="pa-0 custom-scrollbar"
+            style="overflow-y: scroll; height: 350px"
+          >
+            <template v-for="(notification, index) in notification">
+              <v-divider v-if="index != 0" :key="index"></v-divider>
+
+              <v-list-item
+                :key="notification.id"
+                :to="notification.link"
+                :class="notification.seen ? '' : 'light-blue lighten-5'"
+                @click="seenCall(notification.id)"
+              >
+                <!-- class="green"
+                  max-height="20px"
+                  max-width="20px"
+                  min-width="20px"
+                  style="margin-top: 20px" -->
+                <v-list-item-avatar
+                  class="rounded"
+                  max-height="60px"
+                  max-width="60px"
+                  min-width="60px"
+                  height="60px"
+                  width="60px"
+                >
+                  <img
+                    src="~/assets/img/user-img.png"
+                    class="text-center"
+                    height="60"
+                  />
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title
+                    v-html="notification.title"
+                    class="font-weight-medium mb-0"
+                  ></v-list-item-title>
+                  <v-list-item-subtitle
+                    v-html="notification.body"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+          <v-divider></v-divider>
+          <v-list class="py-0 my-0">
+            <v-list-item @click="seeMore()" class="py-3 text-center">
+              <v-list-item-content>
+                <v-list-item-title class="primary--text"
+                  >View All Message</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+
+      <v-menu
+        v-model="menu"
+        :close-on-content-click="false"
+        :nudge-width="200"
+        :nudge-left="300"
+        :offset-overflow="true"
+        :position-x="5000"
+        :position-y="150"
+        :transition="true"
+        max-width="350"
+        min-width="350"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="black"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            icon
+            class="white mr-5 btn-shadow"
+          >
+            <v-badge
+              v-if="notificationUnseen > 0"
+              :content="notificationUnseen"
+              class="badge-notification"
+              color="#31B8B6"
+            ></v-badge>
+            <v-icon size="24">mdi-bell-outline</v-icon>
+          </v-btn>
+        </template>
+        <!-- style="clip-path: polygon(78% 3%, 80% 0, 85% 3%, 100% 3%, 100% 100%, 100% 100%, 0 100%, 0 2%);" -->
+        <div style="width: 100%">
+          <img
+            src="~/assets/img/arrow-up.svg"
+            alt=""
+            style="position: relative; top: 8px; left: 295px"
+          />
+        </div>
+        <v-card id="notification" class="card-shadow">
+          <v-list>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title
+                  class="font-weight-regular subtitle-1 text-center"
+                  >1 new notification</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list
+            three-line
+            class="pa-0 custom-scrollbar"
+            style="overflow-y: scroll; height: 350px"
+          >
+            <template v-for="(notification, index) in notification">
+              <v-divider v-if="index != 0" :key="index"></v-divider>
+
+              <v-list-item
+                :key="notification.id"
+                :to="notification.link"
+                :class="notification.seen ? '' : 'light-blue lighten-5'"
+                @click="seenCall(notification.id)"
+              >
+                <!-- class="green"
+                  max-height="20px"
+                  max-width="20px"
+                  min-width="20px"
+                  style="margin-top: 20px" -->
+                <v-list-item-avatar
+                  class="rounded"
+                  max-height="60px"
+                  max-width="60px"
+                  min-width="60px"
+                  height="60px"
+                  width="60px"
+                >
+                  <img
+                    src="~/assets/img/user-img.png"
+                    class="text-center"
+                    height="60"
+                  />
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title
+                    v-html="notification.title"
+                    class="font-weight-medium mb-0"
+                  ></v-list-item-title>
+                  <v-list-item-subtitle
+                    v-html="notification.body"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+          <v-divider></v-divider>
+          <v-list class="py-0 my-0">
+            <v-list-item @click="seeMore()" class="py-3 text-center">
+              <v-list-item-content>
+                <v-list-item-title class="primary--text"
+                  >View All Notification</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+      <!-- Notification End -->
+      <!-- User link Start -->
+      <v-menu
+        v-model="user_menu"
+        :close-on-content-click="false"
+        :nudge-width="150"
+        offset-x
+        max-width="155"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="white"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            icon
+            class="v-btn-custom-header"
+          >
+            <v-img
+              :src="require('../assets/img/user-img.png')"
+              class="rounded-circle"
+              width="40"
+              height="40"
+            ></v-img>
+            <!-- <v-avatar width="40" height="40">
+              <img v-if="profile.photo" :src="profile.photo.img_url" />
+              <div
+                v-else
+                class="white--text d-flex justify-center align-center"
+                style="height: 100%; width: 100%; background-color: #fb855e"
+              >
+                <span
+                  v-if="profile.first_name && profile.surname"
+                  class="subtitle-1"
+                >
+                  {{ profile.first_name.charAt(0).toUpperCase()
+                  }}{{ profile.surname.charAt(0).toUpperCase() }}
+                </span>
+                <span v-else-if="profile.first_name" class="subtitle-1">
+                  {{ profile.first_name.charAt(0).toUpperCase() }}
+                </span>
+                <span v-else-if="profile.surname" class="subtitle-1"
+                  >{{ profile.surname.charAt(0).toUpperCase() }}
+                </span>
+              </div>
+            </v-avatar> -->
+            <h4 class="ml-2 mr-5" style="color: #555555">Hi, Shuvo</h4>
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-list>
+            <v-list-item to="/setting">
+              <v-list-item-action>
+                <v-icon>mdi-tune-variant</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Setting</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <!-- <v-divider></v-divider> -->
+            <!-- @click="logout" -->
+            <v-list-item>
+              <v-list-item-action>
+                <v-icon>mdi-logout</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title>Logout</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+      <!-- <div>
+        <div><span class="fs-12">shuvoroy@gmail.com</span></div>
+      </div> -->
+      <!-- Notification End -->
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notification: [
+        {
+          id: 4,
+          user_id: 2,
+          title: 'New Staff Added',
+          body: 'New Staff Muhammad Jamil Added! Find Out More!',
+          link: '/clientProfile/7',
+          type: null,
+          platfrom: 'Web',
+          status: 0,
+          seen: 1,
+          seen_at: null,
+          create_time: 1619089785,
+          created_at: '2021-04-22T11:09:45.000000Z',
+          updated_at: '2021-04-22T11:09:45.000000Z',
+          deleted_at: null,
+        },
+        {
+          id: 4,
+          user_id: 2,
+          title: 'New Staff Added',
+          body: 'New Staff Find Out More!',
+          link: '/clientProfile/7',
+          type: null,
+          platfrom: 'Web',
+          status: 0,
+          seen: 1,
+          seen_at: null,
+          create_time: 1619089785,
+          created_at: '2021-04-22T11:09:45.000000Z',
+          updated_at: '2021-04-22T11:09:45.000000Z',
+          deleted_at: null,
+        },
+        {
+          id: 4,
+          user_id: 2,
+          title: 'New Staff Added',
+          body: 'New Staff Muhammad Jamil Added! Find Out More!',
+          link: '/clientProfile/7',
+          type: null,
+          platfrom: 'Web',
+          status: 0,
+          seen: 1,
+          seen_at: null,
+          create_time: 1619089785,
+          created_at: '2021-04-22T11:09:45.000000Z',
+          updated_at: '2021-04-22T11:09:45.000000Z',
+          deleted_at: null,
+        },
+        {
+          id: 4,
+          user_id: 2,
+          title: 'New Staff Added',
+          body: 'New Staff Muhammad Jamil Added',
+          link: '/clientProfile/7',
+          type: null,
+          platfrom: 'Web',
+          status: 0,
+          seen: 1,
+          seen_at: null,
+          create_time: 1619089785,
+          created_at: '2021-04-22T11:09:45.000000Z',
+          updated_at: '2021-04-22T11:09:45.000000Z',
+          deleted_at: null,
+        },
+      ],
+      user_menu: false,
+      menu: false,
+      notificationUnseen: 1,
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Service',
+          to: '/service',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Packege',
+          to: '/packege',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  server: {
+    port: 5000,
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s',
+    title: 'Devute CRM',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        },
+        light: {
+          mprimary: '#2B3897',
+          primary: '#33B8C0',
+          accent: '#c4c9e8',
+          secondary: '#2B3897',
+          info: '#29a1bf',
+          warning: '#e56d48',
+          error: '#f63232',
+          success: '#42d928',
+          grey: '#575857',
+          skyblue: '#F1F4FF',
+          lightblue: '#3E8BFF'
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
